825d219d52d47e771fc23d5478ee33b6d93f598c
jsf2184 <jsf2184@gmail.com>
2020-10-31 15:13:24 -0500

    More Miscellaneous changes

---
+++ src/test/java/com/jsf2184/fb/MyHeapTest.java
@@ -0,0 +1,26 @@
+package com.jsf2184.fb;
+
+import java.util.ArrayList;
+
+public class MyHeapTest {
+
+    public static class Heap {
+        ArrayList<Integer> values;
+
+        public Heap() {
+            this.values = new ArrayList<>();
+            // throw a dummy value  in element 0;
+            values.add(0);
+        }
+
+        public void add(int val) {
+
+        }
+
+        public Integer
+
+
+    }
+
+
+}
